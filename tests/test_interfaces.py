4d4f549a4528596d5d6de5d0db1d8a331d545c64
Remy <107559570+hrszpuk@users.noreply.github.com>
2022-11-18 10:05:43 +0000

    Messing around with pytest

--- tests/test_interfaces.py
+++ tests/test_interfaces.py
@@ -1,1 +1,9 @@
-print("hello")
+from bytespace.interfaces import DatabaseInterface
+
+def test_database_interface_connect():
+    interface = DatabaseInterface("Not a real key")
+    assert interface.connect() == "No username supplied!"
+    assert interface.connect(username="tokorv") == "No password supplied!"
+    assert interface.connect(password="pass34") == "No username supplied!"
+    assert interface.connect(username="ugly", password="nam") == "Requested Application couldnt be found!"
+
