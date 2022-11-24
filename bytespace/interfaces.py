f514473952374a4ce2b3d34bef63d255c8ad0f4f
Remy <107559570+hrszpuk@users.noreply.github.com>
2022-11-24 20:17:52 +0000

    Added Auth and Application interfaces

--- bytespace/interfaces.py
+++ bytespace/interfaces.py
@@ -40,15 +40,69 @@ class DatabaseInterface(Interface):
 
 
 class AuthInterface(Interface):
-    pass
+    def __init__(self, key):
+        super().__init__(key)
+        self.name = "AuthInterface"
+        self.url = self.build_url(f"{self.name}.php")
+
+    def connect(self, token=None):
+
+        data = {"appID": self._key}
+
+        if token is not None:
+            data["token"] = token
+
+        res = requests.post(self.url, data=data)
+
+        return res.text.replace("[SUCCESS] ", "")
+
+    def verify(self, token=None):
+        text = self.connect(token)
+
+        if text == "No Token supplied!":
+            raise InvalidTokenError()
+        elif text == "Requested Application couldnt be found!":
+            raise InvalidAppIDError()
+        else:
+            return True
 
 
 class ApplicationInterface(Interface):
-    pass
+    def __init__(self, key):
+        super().__init__(key)
+        self.name = "ApplicationInterface"
+        self.head = "unique"
+        self.mode = "READUNIQUE"
+        self.url = self.build_url(f"{self.name}.php")
+
+    def connect(self, token=None):
+
+        data = {"appID": self._key}
+
+        if token is not None:
+            data["token"] = token
+
+        res = requests.post(self.url, data=data)
+
+        # Checking response
+        if "[SUCCESS]" not in res:
+            if res.text == "No Token supplied!":
+                raise InvalidTokenError()
+
+            elif res.text == "Requested Application couldnt be found!":
+                raise InvalidAppIDError()
+
+            else:
+                raise BaseInterfaceException()
+
+        return res.text.replace("[SUCCESS]", "")
 
 
 class APITest(Interface):
-    pass
+    def __init__(self, key):
+        super().__init__(key)
+        self.name = "APITest"
+        self.url = self.build_url(f"{self.name}.php")
 
 
 
